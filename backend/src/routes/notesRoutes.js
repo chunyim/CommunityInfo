c4d011b2f5cfb526c67997e2b381dec1164c801a
chunyim <chunhinyim@gmail.com>
2025-08-07 22:28:07 -0400

    for deployment

--- backend/src/routes/notesRoutes.js
+++ backend/src/routes/notesRoutes.js
@@ -10,19 +10,3 @@ router.put("/:id", updateNote)
 router.delete("/:id", deleteNote)
 
 export default router;
-
-// app.get("/api/notes", (req,res) => {
-//     res.status(200).send("you got 10 notes")
-// })
-
-// app.post("/api/notes", (req,res) => {
-//     res.status(201).json({message:"post created successfully"})
-// })
-
-// app.put("/api/notes/:id", (req,res) => {
-//     res.status(200).json({message:"post updated successfully"})
-// })
-
-// app.delete("/api/notes/:id", (req,res) => {
-//     res.status(200).json({message:"post deleted successfully"})
-// })
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,30 +1,41 @@
+import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import express from "express";
+import path from "path";
 
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
+
 const app = express();
 const PORT = process.env.PORT || 5050;
+const __dirname = path.resolve();
 
 // middleware
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-  })
-);
-app.use(express.json());
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
+}
+app.use(express.json()); 
 app.use(rateLimiter);
 
 app.use("/api/notes", notesRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("/*path", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
 connectDB().then(() => {
   app.listen(PORT, () => {
-    console.log("server started on PORT:", PORT);
+    console.log("Server started on PORT:", PORT);
   });
 });
-
-// mongodb+srv://chunhinhenry:DlS7GC3xmvJFgaJE@cluster0.wudex8k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
